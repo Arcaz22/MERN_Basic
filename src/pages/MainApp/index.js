5512bc5b8f0e8fe1fc1a604b1eaf493dc1c3f6fc
Arcaz22 <chandraarcychan@gmail.com>
2023-01-17 22:48:25 +0700

    Styling MainApp, Header dan Footer

--- src/pages/MainApp/index.js
+++ src/pages/MainApp/index.js
@@ -1,19 +1,23 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import { Footer, Header } from "../../components";
 import CreateBlog from "../CreateBlog";
 import DetailBlog from "../DetailBlog";
 import Home from "../Home";
+import "./mainApp.scss";
 
 const MainApp = () => {
   return (
-    <div>
-      <p>Header</p>
-      <Routes>
-        <Route path="/create-blog" element={<CreateBlog />} />
-        <Route path="/detail-blog" element={<DetailBlog />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
-      <p>Footer</p>
+    <div className="main-app-wrapper">
+      <Header />
+      <div className="content-wrapper">
+        <Routes>
+          <Route path="/create-blog" element={<CreateBlog />} />
+          <Route path="/detail-blog" element={<DetailBlog />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+      <Footer />
     </div>
   );
 };
