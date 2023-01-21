c66f43fcb5b87b0894375f2001c6ff997e7682ed
Arcaz22 <chandraarcychan@gmail.com>
2023-01-22 01:46:13 +0700

    init project setup server menggunakan express js install nodemon

--- MERN-Api/index.js
+++ MERN-Api/index.js
@@ -3,7 +3,8 @@ const express = require("express");
 const app = express();
 
 app.use(() => {
-  console.log("");
+  console.log("Hello Server...");
+  console.log("Hello Server....");
 });
 
 app.listen(4000);
