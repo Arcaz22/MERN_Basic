4ce857e780cefb8e8d499d1103065702bd60b03f
Arcaz22 <chandraarcychan@gmail.com>
2023-01-21 15:26:11 +0700

    Styling Halaman Detail Blog Post

--- src/pages/DetailBlog/index.js
+++ src/pages/DetailBlog/index.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { Reglog } from "../../assets";
+import { Gap } from "../../components";
+import "./detail.blog.scss";
 
 const DetailBlog = () => {
   return (
-    <div>
-      <p>Content Detail Blog Page</p>
+    <div className="detail-blog-wrapper">
+      <Gap height={20} />
+      <img className="img-cover" src={Reglog} alt="thumb" />
+      <p className="blog-title">Title Blog</p>
+      <p className="blog-author">Author - Date Post</p>
+      <p className="blog-body">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste quidem
+        recusandae perspiciatis quam quis harum quod aliquid repellat alias
+        soluta esse itaque, maiores repellendus et quisquam qui velit libero vel
+        expedita, saepe sunt error ipsa? Officiis doloremque a dolorum,
+        molestias inventore laborum suscipit ea deserunt quidem incidunt.
+        Minima, eos blanditiis?
+      </p>
     </div>
   );
 };
