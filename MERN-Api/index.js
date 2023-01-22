859baa185acbe0327adc01a5e64fcac7e923c35e
Arcaz22 <chandraarcychan@gmail.com>
2023-01-23 00:35:06 +0700

    Memahami Basic Router, Request dan Response

--- MERN-Api/index.js
+++ MERN-Api/index.js
@@ -1,10 +1,37 @@
 const express = require("express");
 
 const app = express();
+const router = express.Router();
 
-app.use(() => {
-  console.log("Hello Server...");
-  console.log("Hello Server....");
+/* 
+req = Request
+res = Respons
+Next = Mengkakses lebih dari 1
+*/
+router.use("/products", (req, res, next) => {
+  res.json({
+    name: "Chandra Arcychan Azfar",
+    email: "chandraarcychan@gmail.com",
+  });
+  next();
 });
+
+router.use("/price", (req, res, next) => {
+  res.json({
+    price: 1000,
+  });
+  next();
+});
+
+router.delete("/customers", (req, res, next) => {
+  res.json({
+    title: "Customer",
+  });
+  next();
+});
+
+app.use("/", router);
+
+// GET '/users' ==> [{name: Chandra}]
 
 app.listen(4000);
