6b8ee79fb7353db7793f364d7ca2c023b40e82a2
Arcaz22 <chandraarcychan@gmail.com>
2023-02-01 18:05:26 +0700

    Memvalidasi Request Body dan Memberikan Error Response yang Dinamis

--- MERN-Api/src/routes/blog.js
+++ MERN-Api/src/routes/blog.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const { body } = require('express-validator');
 const router = express.Router();
 const blogController = require('../controllers/blog');
 
 // [POST] : /v1/blog/register
-router.post('/post', blogController.createBlogPost);
+router.post('/post', 
+  [
+    body('title').isLength({min: 5}).withMessage('Invalid title'), 
+    body('body').isLength({min: 5}).withMessage('Invalid body'), 
+  ], blogController.createBlogPost);
 
 module.exports = router;
