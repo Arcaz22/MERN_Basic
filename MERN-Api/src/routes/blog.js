5313b615d40565d0c33b08cae03acd81904d06a1
Arcaz22 <chandraarcychan@gmail.com>
2023-02-02 21:07:39 +0700

    Mengambil Seluruh Data Postingan Blog

--- MERN-Api/src/routes/blog.js
+++ MERN-Api/src/routes/blog.js
@@ -9,5 +9,8 @@ router.post('/post',
     body('title').isLength({min: 5}).withMessage('Invalid title'), 
     body('body').isLength({min: 5}).withMessage('Invalid body'), 
   ], blogController.createBlogPost);
+  
+// [GET] : /v1/blog
+router.get('/posts', blogController.getAllBlogPost);
 
 module.exports = router;
